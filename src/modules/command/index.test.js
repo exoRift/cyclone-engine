bd76345a5cc3e922dcd7a19fffd614dfb12914c1
mets11rap <20345400+mets11rap@users.noreply.github.com>
2019-04-19 15:56:58 -0400

    Cover Command where the last arg has a delim

--- src/modules/command/index.test.js
+++ src/modules/command/index.test.js
@@ -10,6 +10,17 @@ const command = new Command({
   action: () => ''
 })
 
+const lastArgDelimCommand = new Command({
+  name: 'ladm',
+  desc: 'Testing when the last arg has a delim',
+  options: {
+    args: [{ name: 'arg', delim: '|' }]
+  },
+  action: () => ''
+})
+
 test('commandInfo', async (t) => {
-  t.is(command.info, '**command <mandatoryArg> <customDelimArg>|(optionalArg)** - *Testing command info*', 'Command info')
+  t.is(command.info, '**command <mandatoryArg> <customDelimArg>|(optionalArg)** - *Testing command info*', 'Regular')
+
+  t.is(lastArgDelimCommand.info, '**ladm (arg)** - *Testing when the last arg has a delim*', 'Last arg has a delim')
 })

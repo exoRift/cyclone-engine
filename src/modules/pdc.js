39fed769b4d2de915b0045bdd2ffdf3ccf3d87a9
Arthur Neuman <20345400+mets11rap@users.noreply.github.com>
2019-04-22 17:39:35 -0400

    Change PDC structure

--- src/modules/pdc.js
+++ src/modules/pdc.js
@@ -1,23 +1,47 @@
-class PseudoDiscordClient {
-  constructor () {
-    this.user = {
-      id: String(Date.now() - 20000)
-    }
+const EventEmitter = require('events').EventEmitter
+
+class PseudoDiscordClient extends EventEmitter {
+  constructor (token, user) {
+    super()
+    this.token = token
+    this.user = user
   }
-  _buildMessage (content, authorId = String(Date.now() - 20000), channelId = String(Date.now() - 10000)) {
-    return {
-      content,
-      id: String(Date.now()),
-      author: {
-        username: 'foo',
-        id: authorId
-      },
-      channel: {
-        id: channelId,
-        createMessage: async () => null
-      }
-    }
+
+  sendMessage () {
+
   }
 }
+
+class Message {
+  constructor (content, author = new User(), channel = new Channel()) {
+    this.id = String(Date.now())
+    this.content = content
+    this.author = author
+    this.channel = channel
+  }
+}
+
+class User {
+  constructor (id = String(Date.now()), username = 'user') {
+    this.id = id
+    this.username = username
+  }
+}
+
+class Channel {
+  constructor (id = String(Date.now()), name = 'channel') {
+    this.id = id
+    this.name = name
+    this.messages = new Map()
+  }
+
+  async createMessage () {
+    return true
+  }
+}
+
+PseudoDiscordClient.Message = Message
+PseudoDiscordClient.User = User
+PseudoDiscordClient.Channel = Channel
 
 module.exports = PseudoDiscordClient

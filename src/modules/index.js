be6ef9c102476a9a710e4225e7d51223998a3555
Arthur Neuman <20345400+mets11rap@users.noreply.github.com>
2019-06-27 15:59:10 -0400

    Add new class structures to indices

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,6 +2,8 @@ module.exports = {
   Command: require('./command'),
   Await: require('./await'),
   Replacer: require('./replacer'),
+  ReactCommand: require('./react-command'),
+  ReactInterface: require('./react-interface'),
   _CommandHandler: require('./command-handler'),
   _ReactionHandler: require('./reaction-handler')
 }

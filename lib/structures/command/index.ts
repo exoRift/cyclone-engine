3ec9378f1221d90678f73fe85ae83afeb3987aa0
Arthur Neuman <arthurbneuman@gmail.com>
2022-12-08 11:52:23 -0500

    Rewrite command structure

---
+++ lib/structures/command/index.ts
@@ -0,0 +1,144 @@
+/** The type of an argument and how it's received */
+enum ArgumentType {
+  STRING,
+  NUMBER,
+  USER,
+  CHANNEL
+}
+
+/** Power levels for permissions */
+enum AuthLevel {
+  MEMBER,
+  ADMIN,
+  OWNER
+}
+
+/** Command Argument */
+interface Argument {
+  name: string, /** The name of the argument */
+  required?: boolean, /** Whether the argument is required for the command to be run or not */
+  type?: ArgumentType, /** What type of argument this is */
+  delimiter?: string /** What separates this argument from the next one */
+}
+
+/** Command options */
+interface CommandOptions {
+  aliases?: string[], /** A list of aliases for the command */
+  guide?: GuideOptions, /** Extra data for how the command appears in the guide */
+  clearance?: AuthLevel, /** The clearance level required for a user to run this command */
+  restricted?: boolean /** Whether this command is restricted to the owner of the bot or not */
+} // todo: define GuideOptions
+
+/** Input data for commands */
+interface CommandData {
+  name: string, /** The name of the command */
+  description?: string, /** A description of the command */
+  options: CommandOptions, /** Miscellaneous options for the command */
+  args: Argument[], /** A list of the command's arguments */
+  action: CommandAction /** The action for the command to execute */
+} // todo: define CommandAction
+
+class Command {
+  /**
+   * The prefixes for arguments depending on type
+   */
+  static _typePrefixes = {
+    [ArgumentType.STRING]: '',
+    [ArgumentType.NUMBER]: '#',
+    [ArgumentType.USER]: '@',
+    [ArgumentType.CHANNEL]: '[#]'
+  }
+
+  /**
+   * Return a blurb for an argument
+   * @param   {Argument} arg The argument
+   * @returns {String}
+   */
+  static getArgBlurb (arg: Required<Argument>): string {
+    const inside = `${Command._typePrefixes[arg.type] || ''}${arg.name}`
+
+    return arg.required ? `<${inside}>` : `(${inside})`
+  }
+
+  name: string
+  description: string
+  options: Required<CommandOptions>
+  args: Required<Argument>[] = []
+  action: CommandAction
+
+  /**
+   * Construct a Command
+   * @param {CommandData} data The data for the command
+   */
+  constructor (data: CommandData) {
+    const {
+      name,
+      description = '',
+      options = {},
+      args = [],
+      action
+    }: CommandData = data
+
+    const {
+      aliases = [],
+      guide = {},
+      clearance = AuthLevel.MEMBER,
+      restricted = false
+    }: CommandOptions = options
+
+    this.name = name.toLowerCase()
+    this.description = description
+
+    this.options = {
+      aliases,
+      guide,
+      clearance,
+      restricted
+    }
+
+    this._digestArguments(args)
+    this.action = action.bind(this)
+  }
+
+  /**
+   * Get a blurb string describing the command
+   * @type {String}
+   * @example       '**name**|**alias** **<mandatory arg> (optional arg) (#number arg)** - *description*'
+   */
+  get blurb (): string {
+    const name = `**${this.name}**` + this.options.aliases.reduce((accum, alias) => `${accum}|**${alias}**`, '')
+
+    const args = this.args.reduce((accum, arg, index) => {
+      const lastArg = index === this.args.length - 1
+
+      return Command.getArgBlurb(arg) + (lastArg ? '' : arg.delimiter)
+    }, '')
+
+    return `${this.options.restricted ? '~~' : ''}${name}${args ? ` **${args}**` : ''} - *${this.description}*${this.options.restricted ? '~~' : ''}`
+  }
+
+  /**
+   * Digest provided argument data into consistent argument blocks
+   * @param {Argument[]} args The arguments
+   */
+  _digestArguments (args: Argument[]): void {
+    for (const arg of args) {
+      this.args.push({
+        name: arg.name.toLowerCase(),
+        required: arg.required || false,
+        type: arg.type || ArgumentType.STRING,
+        delimiter: arg.delimiter || ' '
+      })
+    }
+  }
+}
+
+export default Command
+
+export {
+  Argument,
+  AuthLevel,
+  Command,
+  CommandOptions,
+  ArgumentType
+}

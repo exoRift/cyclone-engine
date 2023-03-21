0d628c66c0a2aa607c885f20bdf6ad8a74a43a30
Arthur Neuman <arthurbneuman@gmail.com>
2023-03-21 10:26:01 -0400

    Upgrade to TS 5.0

--- lib/structures/operation/message.ts
+++ lib/structures/operation/message.ts
@@ -12,7 +12,7 @@ export type MessageOperationData = Oceanic.CreateMessageOptions
 export class Message extends Base<MessageOperationData> {
   readonly type = 'operation'
 
-  private _getDestination (origin: Origin): Oceanic.AnyTextChannelWithoutGroup | Oceanic.CommandInteraction | undefined {
+  private _getDestination (origin: Origin): Oceanic.AnyTextChannelWithoutGroup | Oceanic.AnyInteractionGateway | undefined {
     switch (origin.type) {
       case 'channel': return origin.value
       case 'interaction': return origin.value
@@ -22,6 +22,9 @@ export class Message extends Base<MessageOperationData> {
 
   async execute (target: Origin): Promise<Origin | void> {
     const destination = this._getDestination(target)
+
+    // TODO: Make sure this never happens
+    if (destination?.type === Oceanic.InteractionTypes.APPLICATION_COMMAND_AUTOCOMPLETE) throw Error('This interaction is unrespondable')
 
     return await destination?.createMessage(this.data) // todo: permission checking and catch
       .then((msg) => {
